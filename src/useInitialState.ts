11e463d0860ed20afdde808ce72acc44975d223d
Yasser <yas@keemail.me>
2020-01-26 13:14:36 +0200

    added setState callback function

--- src/useInitialState.ts
+++ src/useInitialState.ts
@@ -3,7 +3,7 @@ import { useReducer } from 'react';
 const mini_reducer = (state = {}, action) => ({ ...state, [action.type]: action.payload });
 const useInitialState = (initialState: object = {}) => {
     const [state, dispatch] = useReducer(mini_reducer, initialState);
-    const setState = (fState = {}) => { for (const type in fState) { dispatch({ type, payload: fState[type] }); } };
+    const setState = (fState = {}, callback = () => null) => { for (const type in fState) { dispatch({ type, payload: fState[type] }); } callback(); };
     const resetState = () => setState(initialState);
     return { state, setState, resetState };
 };
